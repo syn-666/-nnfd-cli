b8484779b707307b714de7304828d7aaf2c48dec
LAPTOP-EDB23I61\史雅楠 <1599386309@qq.com>
2023-05-18 12:01:43 +0800

    更换child_process 多平台适配

--- bin/index.js
+++ bin/index.js
@@ -1,42 +1,36 @@
 #! /usr/bin/env node
-import { execa } from "execa";
+import { exec } from "child_process";
 import { program } from "commander";
 
 
 program
 	.name("nn cli")
 	.description("a simple cli by nn")
-	.version("0.0.1", '-v');
+	.version("0.0.3", '-v');
 
 program
 	.command("wechat")
 	.description("打开微信小程序文档")
-	.action(async () => {
-        await execa('open', ['https://developers.weixin.qq.com/miniprogram/dev/framework/'])
-	});
-
-program
-	.command("my")
-	.description("打开微信小程序文档")
-	.action(async () => {
-        await execa('open', ['https://opendocs.alipay.com/mini/framework'])
+	.action(() => {
+		exec(`${process.platform === 'darwin' ? 'open' : 'start'} https://developers.weixin.qq.com/miniprogram/dev/framework/`)
 	});
 
 program
 	.command("my")
 	.description("打开支付宝小程序文档")
-	.action(async () => {
-        await execa('open', ['https://opendocs.alipay.com/mini/framework'])
+	.action(() => {
+		exec(`${process.platform === 'darwin' ? 'open' : 'start'} https://opendocs.alipay.com/mini/framework`)
 	});
 
 program
 	.command("mdn")
 	.description("打开MDN文档")
-	.action(async () => {
-        await execa('open', ['https://developer.mozilla.org/zh-CN/docs/Web/JavaScript/Reference'])
+	.action(() => {
+		exec(`${process.platform === 'darwin' ? 'open' : 'start'} https://developer.mozilla.org/zh-CN/docs/Web/JavaScript/Reference`)
 	});
 
     
     
 program.parse(process.argv);
+
 
